c2e123d049c911ffc876b0f3f19fd8996ddc9079
DiogoAlip <diogoalipazaga@gmail.com>
2026-03-02 13:38:32 -0500

    feat: axios and TanStackQuery

--- src/GoDevApp.tsx
+++ src/GoDevApp.tsx
@@ -1,6 +1,15 @@
 import { AppRouter } from "./app.router.tsx";
 import { RouterProvider } from "react-router";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export const GoDevApp = () => {
-  return <RouterProvider router={AppRouter} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={AppRouter} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 };
