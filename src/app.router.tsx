09c7498723f4d235332cb244752e47a7ee481bf3
DiogoAlip <diogoalipazaga@gmail.com>
2026-03-27 17:57:28 -0500

    fix: path to admin route

--- src/app.router.tsx
+++ src/app.router.tsx
@@ -5,10 +5,8 @@ import { ShopLayout } from "./shop/layouts/ShopLayout";
 import { HomePage } from "./shop/pages/home/HomePage";
 import { ProductPage } from "./shop/pages/product/ProductPage";
 import { GenderPage } from "./shop/pages/gender/GenderPage";
-//import { AuthLayout } from "./auth/layouts/AuthLayout";
 import { LoginPage } from "./auth/pages/login/LoginPage";
 import { Register } from "./auth/pages/register/Register";
-//import { AdminLayout } from "./admin/layouts/AdminLayout";
 import { DashboardPage } from "./admin/pages/dashboard/DashboardPage";
 import { AdminProductsPage } from "./admin/pages/products/AdminProductsPage";
 import { AdminProductPage } from "./admin/pages/product/AdminProductPage";
@@ -62,7 +60,7 @@ export const AppRouter = createHashRouter([
     ],
   },
   {
-    path: "admin",
+    path: "/admin",
     element: (
       <AdminRoute>
         <AdminLayout />
